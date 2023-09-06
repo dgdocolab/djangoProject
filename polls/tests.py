c6c875b0a23726d221d6bfe1588f429e25e4d816
sddocolab <stagiaire-developpement@docolab.fr>
2023-09-06 17:37:48 +0200

    - UPDATE tests.py, setup fixtures;

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,18 @@
+import datetime
+
 from django.test import TestCase
+
+from polls.models import Question
+
+
+# exemple
+# question_list = [
+#     "Salut ça va ?",
+# ]
+
+def create_question(text, date, author=None):
+    # check text, date, author
+    return Question.objects.create(question_text=text, pub_date=date, author=author)
 
 
 class DevTest(TestCase):
@@ -6,6 +20,12 @@ class DevTest(TestCase):
 
 
 class QuestionTest(TestCase):
+    # def test_nul_a_chier(self):
+    #     question_1 = create_question("bla", datetime.datetime.now().astimezone(), "jean kulowski")
+    #     question_2 = create_question("bla 2", datetime.datetime.now().astimezone(), "jean kulowski")
+    #     print(Question.objects.all())
+    #     print(type(Question.objects.all()))
+    #     print(Question.objects.filter(author="jean kulowski"))
     pass
 
 
@@ -15,3 +35,11 @@ class ChoiceTest(TestCase):
 
 class UtilitiesTest(TestCase):
     pass
+
+
+"""
+script devtest , print pour verifier ( toute les questions)
+def (test_ + nom parlant + signature et courte)
+
+
+"""
